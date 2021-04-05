10fa701fad2602ba4ad2fd9360f92c0fbcbf8e2f
Nicole Dreon <dreon.nicole@gmail.com>
2021-04-05 20:27:50 +0000

    change name for class to singular and table name to plural for morning

--- model.py
+++ model.py
@@ -23,10 +23,10 @@ class User(db.Model):
         return f'< User user_id = {self.user_id} fname = {self.fname} lname = {self.lname} email = {self.email}, password = {self.password} dob = {self.dob} gender = {self.gender} >'
 
 
-class Morning_Entries(db.Model):
+class Morning_Entry(db.Model):
     """A morning entry."""
 
-    __tablename__ = 'morning_entry'
+    __tablename__ = 'morning_entries'
 
     am_entry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForignKey('users.user_id'))
@@ -37,7 +37,7 @@ class Morning_Entries(db.Model):
     journal_entry = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<Morning_Entries user_id={self.user_id} date={self.date} hrs_sleep={self.hrs_sleep} qual_sleep={self.qual_sleep} goal={self.goal} journal_entry={self.journal_entry}>'
+        return f'<Morning_Entry user_id={self.user_id} date={self.date} hrs_sleep={self.hrs_sleep} qual_sleep={self.qual_sleep} goal={self.goal} journal_entry={self.journal_entry}>'
 
 
 if __name__ == '__main__':
