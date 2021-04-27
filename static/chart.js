5421fadaea5948da84027fb7af7e433e81679ef3
Nicole Dreon <dreon.nicole@gmail.com>
2021-04-27 19:37:36 +0000

    place holder chart working

--- static/chart.js
+++ static/chart.js
@@ -1,16 +1,16 @@
 "use strict";
 
-// const options = {
-//   responsive: true,
-// };
+const options = {
+  responsive: true,
+};
 
-// let ctx = $("#lineChart").get(0).getContext("2d");
+let ctx = $("#lineChart").get(0).getContext("2d");
 
-// $.get("/chart.json", function (data) {
-//   let myLineChart = new Chart(ctx, {
-//     type: "line",
-//     data: data,
-//     options: options,
-//   });
-//   $("#lineLegend").html(myLineChart.generateLegend());
-// });
+$.get("/chart.json", function (data) {
+  let myLineChart = new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: options,
+  });
+  $("#lineLegend").html(myLineChart.generateLegend());
+});
