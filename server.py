f08979f8bfe1c53a8ab2382f306e74690d9c6c33
Nicole Dreon <dreon.nicole@gmail.com>
2021-04-27 19:22:39 +0000

    add route to server to display chart, add chart.html and chart.js

--- server.py
+++ server.py
@@ -218,6 +218,65 @@ def add_missing_pm_entry():
 
     return render_template('add_missing_pm_entry.html', date=date_formated)
 
+@app.route('/chart.json')
+def chart():
+    """Send data to front end."""
+
+    data_dict = {
+        "labels": ["January", "February", "March", "April", "May", "June", "July"],
+        "datasets": [
+            {
+                "label": "Watermelon",
+                "fill": True,
+                "lineTension": 0.5,
+                "backgroundColor": "rgba(220,220,220,0.2)",
+                "borderColor": "rgba(220,220,220,1)",
+                "borderCapStyle": 'butt',
+                "borderDash": [],
+                "borderDashOffset": 0.0,
+                "borderJoinStyle": 'miter',
+                "pointBorderColor": "rgba(220,220,220,1)",
+                "pointBackgroundColor": "#fff",
+                "pointBorderWidth": 1,
+                "pointHoverRadius": 5,
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgba(220,220,220,1)",
+                "pointHoverBorderWidth": 2,
+                "pointRadius": 3,
+                "pointHitRadius": 10,
+                "data": [65, 59, 80, 81, 56, 55, 40],
+                "spanGaps": False},
+            {
+                "label": "Cantaloupe",
+                "fill": True,
+                "lineTension": 0.5,
+                "backgroundColor": "rgba(151,187,205,0.2)",
+                "borderColor": "rgba(151,187,205,1)",
+                "borderCapStyle": 'butt',
+                "borderDash": [],
+                "borderDashOffset": 0.0,
+                "borderJoinStyle": 'miter',
+                "pointBorderColor": "rgba(151,187,205,1)",
+                "pointBackgroundColor": "#fff",
+                "pointBorderWidth": 1,
+                "pointHoverRadius": 5,
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgba(151,187,205,1)",
+                "pointHoverBorderWidth": 2,
+                "pointHitRadius": 10,
+                "data": [28, 48, 40, 19, 86, 27, 90],
+                "spanGaps": False}
+        ]
+    }
+    return jsonify(data_dict)
+    
+
+@app.route('/display_chart')
+def display_chart():
+    """Display chart of user data."""
+
+    return render_template('chart.html')
+
 
 @app.route('/logout')
 def logout():
