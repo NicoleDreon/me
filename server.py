425a60470894eb6879562189db13202acaa6ae2a
Nicole Dreon <dreon.nicole@gmail.com>
2021-04-28 20:08:00 +0000

    remove test data

--- server.py
+++ server.py
@@ -231,52 +231,7 @@ def chart():
             data_dict[entry.date.strftime('%Y-%m-%d')] = {'qual_day': str(entry.qual_day), 'activity_level': entry.activity_level, 'goal_completed': entry.goal_completed}
 
     print(data_dict)
-    # data_dict = {
-    #     "labels": ["January", "February", "March", "April", "May", "June", "July"],
-    #     "datasets": [
-    #         {
-    #             "label": "Watermelon",
-    #             "fill": True,
-    #             "lineTension": 0.5,
-    #             "backgroundColor": "rgba(220,220,220,0.2)",
-    #             "borderColor": "rgba(220,220,220,1)",
-    #             "borderCapStyle": 'butt',
-    #             "borderDash": [],
-    #             "borderDashOffset": 0.0,
-    #             "borderJoinStyle": 'miter',
-    #             "pointBorderColor": "rgba(220,220,220,1)",
-    #             "pointBackgroundColor": "#fff",
-    #             "pointBorderWidth": 1,
-    #             "pointHoverRadius": 5,
-    #             "pointHoverBackgroundColor": "#fff",
-    #             "pointHoverBorderColor": "rgba(220,220,220,1)",
-    #             "pointHoverBorderWidth": 2,
-    #             "pointRadius": 3,
-    #             "pointHitRadius": 10,
-    #             "data": [65, 59, 80, 81, 56, 55, 40],
-    #             "spanGaps": False},
-    #         {
-    #             "label": "Cantaloupe",
-    #             "fill": True,
-    #             "lineTension": 0.5,
-    #             "backgroundColor": "rgba(151,187,205,0.2)",
-    #             "borderColor": "rgba(151,187,205,1)",
-    #             "borderCapStyle": 'butt',
-    #             "borderDash": [],
-    #             "borderDashOffset": 0.0,
-    #             "borderJoinStyle": 'miter',
-    #             "pointBorderColor": "rgba(151,187,205,1)",
-    #             "pointBackgroundColor": "#fff",
-    #             "pointBorderWidth": 1,
-    #             "pointHoverRadius": 5,
-    #             "pointHoverBackgroundColor": "#fff",
-    #             "pointHoverBorderColor": "rgba(151,187,205,1)",
-    #             "pointHoverBorderWidth": 2,
-    #             "pointHitRadius": 10,
-    #             "data": [28, 48, 40, 19, 86, 27, 90],
-    #             "spanGaps": False}
-    #     ]
-    # }
+    
     return jsonify(data_dict)
     
 
